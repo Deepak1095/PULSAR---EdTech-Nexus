b99cedd8b8db6a45272341d4deac47811cb8247a
Deepak1095 <ddhankhar999@gmail.com>
2023-08-31 07:20:16 +0530

    create all urls endPoints and functions

---
+++ EdTechNexus/eduapp/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Instructor URLs
+    path('instructors/', views.list_instructors, name='list-instructors'),
+    path('instructors/create/', views.create_instructor, name='create-instructor'),
+
+    # Student URLs
+    path('students/', views.list_students, name='list-students'),
+    path('students/create/', views.create_student, name='create-student'),
+
+    # Course URLs
+    path('courses/', views.list_courses, name='list-courses'),
+    path('courses/create/', views.create_course, name='create-course'),
+
+    # Assignment URLs
+    path('assignments/', views.list_assignments, name='list-assignments'),
+    path('assignments/create/', views.create_assignment, name='create-assignment'),
+
+    # Enrollment URLs
+    path('enrollments/', views.list_enrollments, name='list-enrollments'),
+    path('enrollments/create/', views.create_enrollment, name='create-enrollment'),
+
+    # Submission URLs
+    path('submissions/', views.list_submissions, name='list-submissions'),
+    path('submissions/create/', views.create_submission, name='create-submission'),
+
+    # Department URLs
+    path('departments/', views.list_departments, name='list-departments'),
+    path('departments/create/', views.create_department, name='create-department'),
+
+    # Announcement URLs
+    path('announcements/', views.list_announcements, name='list-announcements'),
+    path('announcements/create/', views.create_announcement, name='create-announcement'),
+]
--- EdTechNexus/eduapp/views.py
+++ EdTechNexus/eduapp/views.py
@@ -1,1 +1,75 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Instructor, Student, Course, Assignment, Enrollment, Submission, Department, Announcement
+
+# Instructor views
+@csrf_exempt
+def create_instructor(request):
+    return HttpResponse('create_instructor')
+
+@csrf_exempt
+def list_instructors(request):
+    return HttpResponse('list_instructors')
+
+# Student views
+@csrf_exempt
+def create_student(request):
+    return HttpResponse('create_student')
+
+@csrf_exempt
+def list_students(request):
+    return HttpResponse('list_students')
+
+# Course views
+@csrf_exempt
+def create_course(request):
+    return HttpResponse('create_course')
+
+@csrf_exempt
+def list_courses(request):
+    return HttpResponse('list_courses')
+
+# Assignment views
+@csrf_exempt
+def create_assignment(request):
+    return HttpResponse('create_assignment')
+
+@csrf_exempt
+def list_assignments(request):
+    return HttpResponse('list_assignments')
+
+# Enrollment views
+@csrf_exempt
+def create_enrollment(request):
+    return HttpResponse('create_enrollment')
+
+@csrf_exempt
+def list_enrollments(request):
+    return HttpResponse('list_enrollments')
+
+# Submission views
+@csrf_exempt
+def create_submission(request):
+    return HttpResponse('create_submission')
+
+@csrf_exempt
+def list_submissions(request):
+    return HttpResponse('list_submissions')
+
+# Department views
+@csrf_exempt
+def create_department(request):
+    return HttpResponse('create_department')
+
+@csrf_exempt
+def list_departments(request):
+    return HttpResponse('list_departments')
+
+# Announcement views
+@csrf_exempt
+def create_announcement(request):
+    return HttpResponse('create_announcement')
+
+@csrf_exempt
+def list_announcements(request):
+    return HttpResponse('list_announcements')
