d5d2f1d083ce4755ebf202952908069045b56a67
Deepak1095 <ddhankhar999@gmail.com>
2023-08-31 07:52:15 +0530

    add frontend

---
+++ edtechnexusfrontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InstructorManagementComponent } from './instructor-management/instructor-management.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InstructorManagementComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
